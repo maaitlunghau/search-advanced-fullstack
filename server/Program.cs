d3b10f00c715a601a2f56259d9f847fb405f6ab4
maaitlunghau <chunhau.py@gmail.com>
2026-03-18 14:19:24 +0700

    feat: configure connection string to MySQL

--- server/Program.cs
+++ server/Program.cs
@@ -1,8 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using server.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("ConnectedDB"),
+        ServerVersion.AutoDetect(builder.Configuration
+        .GetConnectionString("ConnectedDB"))
+    ));
 
 var app = builder.Build();
 
@@ -21,7 +30,7 @@ var summaries = new[]
 
 app.MapGet("/weatherforecast", () =>
 {
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
+    var forecast = Enumerable.Range(1, 5).Select(index =>
         new WeatherForecast
         (
             DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
