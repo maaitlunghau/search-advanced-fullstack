61af840e811ed475ba3371cf5ce17697de99a871
maaitlunghau <chunhau.py@gmail.com>
2026-03-19 23:08:11 +0700

    feat(server): update CORS policy

--- server/Program.cs
+++ server/Program.cs
@@ -3,23 +3,39 @@ using server.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddDbContext<DataContext>(options =>
     options.UseMySql(
         builder.Configuration.GetConnectionString("ConnectedDB"),
-        ServerVersion.AutoDetect(builder.Configuration
-        .GetConnectionString("ConnectedDB"))
+        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("ConnectedDB"))
     ));
+
+builder.Services.AddControllers();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend", policy =>
+    {
+        policy.WithOrigins(
+                "http://localhost:3000",
+                "http://localhost:3001"
+              )
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Run Seeder on startup (development only)
 if (app.Environment.IsDevelopment())
 {
-    // 
+    using var scope = app.Services.CreateScope();
+    var db = scope.ServiceProvider.GetRequiredService<DataContext>();
+    await DataSeeder.SeedAsync(db);
 }
 
+app.UseCors("AllowFrontend");
 app.UseHttpsRedirection();
+app.MapControllers();
 
 app.Run();
